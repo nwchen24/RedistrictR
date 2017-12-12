54320d7d22bbedbdcb2b81bcb175b82db74a6429
Nicole Lee <nicolelee394@gmail.com>
2017-12-11 19:16:55 -0800

    new highlighting interactivity

--- Shiny App/server.R
+++ Shiny App/server.R
@@ -47,16 +47,16 @@ getTables = function(host="redistrictr.cdm5j7ydnstx.us-east-1.rds.amazonaws.com"
   
   # read tables
   return(list(assignments = tbl(my_db, "assignments"),
-              solutions = as.data.frame(tbl(my_db, "solutions")),
-              targets = as.data.frame(tbl(my_db, "targets"))))
+              solutions = tbl(my_db, "solutions"),
+              targets = tbl(my_db, "targets")))
 }
 
 
 # get tables from rdb
 tables = getTables()
-a = tables$assignments # assignments per solution_id
-s = tables$solutions # solution_id, target_id, calculations
-t = tables$targets # target_id & what is being optimized
+a = as.data.frame(tables$assignments) # assignments per solution_id
+s = as.data.frame(tables$solutions) # solution_id, target_id, calculations
+t = as.data.frame(tables$targets) # target_id & what is being optimized
 
 # get base data
 load("./SD_all_data.RData")
@@ -71,6 +71,7 @@ server <- function(input, output, session) {
     print("clicked")
     updateTabsetPanel(session, "redistrictR",
                      selected = "Application")
+    showNotification(paste("Please give me some time to load!"), type="message", duration=2)
   })
   
   # take the solution set that is optimized for the selected optimization factor (optfactor) (100 solutions) 
@@ -78,7 +79,7 @@ server <- function(input, output, session) {
   solution_subset = reactive({
     target = t[t[,2]== (if ("compactness" %in% input$optfactor) 1 else 0) & t[,3]== (if ("vote_efficiency" %in% input$optfactor) 1 else 0) & t[,4] == (if ("cluster_proximity" %in% input$optfactor) 1 else 0),]
     return(s[s$target_id==target$id,][order(s[s$target_id==target$id,][,"fitness"], decreasing=T),])
-    
+    print("getting solution subset")
   })
   
   # get the data for the county selected
@@ -90,15 +91,207 @@ server <- function(input, output, session) {
     function(sol_id) {
       subset = filter(a, solution_id == sol_id) %>% collect()
       return(as.data.frame(subset[,c("geoid","assignment")]))
+      print('getting assignments')
     }
   })
   
   map_theme = providers$CartoDB.DarkMatterNoLabels
   
+#  outputOptions(output, compactness, suspendWhenHidden=FALSE)
+  
+  ### ZOOMING IN WHEN CLICK ONE MAP
+  # clicked_map = reactiveValues()
+  # clicked_map$map = NA
+  
+  # observeEvent(input$map5_shape_click, {
+  #   print("zooming")
+  #   updateTabsetPanel(session, "redistrictR",
+  #                     selected = "Zoom")
+  #   output$map_select = "map5"
+  # })
+  # 
+  # observeEvent(input$map6_shape_click, {
+  #   print("zooming")
+  #   updateTabsetPanel(session, "redistrictR",
+  #                     selected = "Zoom")
+  #   output$map_select = output$map6
+  # })
+  
+  ## HIGHLIGHTING BARS ON LEFT CHARTS
+  cols_base = rgb(1,1,1,alpha=0.5)
+  bar_cols = reactiveValues()
+  bar_cols$compactness = cols_base
+  bar_cols$vote_efficiency = cols_base
+  bar_cols$cluster_proximity = cols_base
+  
+  
+  ## HIGHLIGHT COMPACTNESS CHART BASED ON MOUSEOVER OF PLOT
+  observeEvent(input$map1_shape_mouseover, {
+    # m = input$map1_shape_mouseover
+    # values$map1_toggle = TRUE
+    # print(values$map1_toggle)
+    
+    sub_comp = solution_subset() %>% group_by("c" = round(compactness,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[1,]$compactness,2))
+    bar_cols$compactness = rep(cols_base, nrow(sub_comp))
+    bar_cols$compactness[ind] = "yellow"
+    names(bar_cols$compactness) = sub_comp$c
+  })
+  
+  observeEvent(input$map2_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(compactness,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[2,]$compactness,2))
+    bar_cols$compactness = rep(cols_base, nrow(sub_comp))
+    bar_cols$compactness[ind] = "yellow"
+    names(bar_cols$compactness) = sub_comp$c
+  })
+  
+  observeEvent(input$map3_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(compactness,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[3,]$compactness,2))
+    bar_cols$compactness = rep(cols_base, nrow(sub_comp))
+    bar_cols$compactness[ind] = "yellow"
+    names(bar_cols$compactness) = sub_comp$c
+  })
+  
+  observeEvent(input$map4_shape_mouseover, {
+    
+    sub_comp = solution_subset() %>% group_by("c" = round(compactness,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[4,]$compactness,2))
+    bar_cols$compactness = rep(cols_base, nrow(sub_comp))
+    bar_cols$compactness[ind] = "yellow"
+    names(bar_cols$compactness) = sub_comp$c
+  })
+  
+  observeEvent(input$map5_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(compactness,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[5,]$compactness,2))
+    bar_cols$compactness = rep(cols_base, nrow(sub_comp))
+    bar_cols$compactness[ind] = "yellow"
+    names(bar_cols$compactness) = sub_comp$c
+  })
+  
+  observeEvent(input$map6_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(compactness,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[6,]$compactness,2))
+    bar_cols$compactness = rep(cols_base, nrow(sub_comp))
+    bar_cols$compactness[ind] = "yellow"
+    names(bar_cols$compactness) = sub_comp$c
+  })
+  
+  #######
+  
+  
+  ## HIGHLIGHT vote_efficiency CHART BASED ON MOUSEOVER OF PLOT
+  observeEvent(input$map1_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(vote_efficiency,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[1,]$vote_efficiency,2))
+    bar_cols$vote_efficiency = rep(cols_base, nrow(sub_comp))
+    bar_cols$vote_efficiency[ind] = "yellow"
+    names(bar_cols$vote_efficiency) = sub_comp$c
+  })
+  
+  observeEvent(input$map2_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(vote_efficiency,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[2,]$vote_efficiency,2))
+    bar_cols$vote_efficiency = rep(cols_base, nrow(sub_comp))
+    bar_cols$vote_efficiency[ind] = "yellow"
+    names(bar_cols$vote_efficiency) = sub_comp$c
+  })
+  
+  observeEvent(input$map3_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(vote_efficiency,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[3,]$vote_efficiency,2))
+    bar_cols$vote_efficiency = rep(cols_base, nrow(sub_comp))
+    bar_cols$vote_efficiency[ind] = "yellow"
+    names(bar_cols$vote_efficiency) = sub_comp$c
+  })
+  
+  observeEvent(input$map4_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(vote_efficiency,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[4,]$vote_efficiency,2))
+    bar_cols$vote_efficiency = rep(cols_base, nrow(sub_comp))
+    bar_cols$vote_efficiency[ind] = "yellow"
+    names(bar_cols$vote_efficiency) = sub_comp$c
+  })
+  
+  observeEvent(input$map5_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(vote_efficiency,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[5,]$vote_efficiency,2))
+    bar_cols$vote_efficiency = rep(cols_base, nrow(sub_comp))
+    bar_cols$vote_efficiency[ind] = "yellow"
+    names(bar_cols$vote_efficiency) = sub_comp$c
+  })
+  
+  observeEvent(input$map6_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(vote_efficiency,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[6,]$vote_efficiency,2))
+    bar_cols$vote_efficiency = rep(cols_base, nrow(sub_comp))
+    bar_cols$vote_efficiency[ind] = "yellow"
+    names(bar_cols$vote_efficiency) = sub_comp$c
+  })
+
+  #######
+  
+  
+  ## HIGHLIGHT cluster_proximity CHART BASED ON MOUSEOVER OF PLOT
+  observeEvent(input$map1_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(cluster_proximity,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[1,]$cluster_proximity,2))
+    bar_cols$cluster_proximity = rep(cols_base, nrow(sub_comp))
+    bar_cols$cluster_proximity[ind] = "yellow"
+    names(bar_cols$cluster_proximity) = sub_comp$c
+  })
+  
+  observeEvent(input$map2_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(cluster_proximity,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[2,]$cluster_proximity,2))
+    bar_cols$cluster_proximity = rep(cols_base, nrow(sub_comp))
+    bar_cols$cluster_proximity[ind] = "yellow"
+    names(bar_cols$cluster_proximity) = sub_comp$c
+  })
+  
+  observeEvent(input$map3_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(cluster_proximity,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[3,]$cluster_proximity,2))
+    bar_cols$cluster_proximity = rep(cols_base, nrow(sub_comp))
+    bar_cols$cluster_proximity[ind] = "yellow"
+    names(bar_cols$cluster_proximity) = sub_comp$c
+  })
+  
+  observeEvent(input$map4_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(cluster_proximity,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[4,]$cluster_proximity,2))
+    bar_cols$cluster_proximity = rep(cols_base, nrow(sub_comp))
+    bar_cols$cluster_proximity[ind] = "yellow"
+    names(bar_cols$cluster_proximity) = sub_comp$c
+  })
+  
+  observeEvent(input$map5_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(cluster_proximity,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[5,]$cluster_proximity,2))
+    bar_cols$cluster_proximity = rep(cols_base, nrow(sub_comp))
+    bar_cols$cluster_proximity[ind] = "yellow"
+    names(bar_cols$cluster_proximity) = sub_comp$c
+  })
+  
+  observeEvent(input$map6_shape_mouseover, {
+    sub_comp = solution_subset() %>% group_by("c" = round(cluster_proximity,2)) %>% count()
+    ind = which(sub_comp$c==round(solution_subset()[6,]$cluster_proximity,2))
+    bar_cols$cluster_proximity = rep(cols_base, nrow(sub_comp))
+    bar_cols$cluster_proximity[ind] = "yellow"
+    names(bar_cols$cluster_proximity) = sub_comp$c
+  })
+  
+  #######
+  
+  
+  
+  ### RESULT DISTRIBUTIONS ON LEFT PANEL
   output$compactness = renderPlotly({
-    p = ggplot(solution_subset(), aes(round(compactness,2))) +
+    p = ggplot(solution_subset(), aes(x=round(compactness,2), text=paste(..count..,"potential maps with","<br>","compactness of",x))) +
       xlab("Compactness Score") +
-      geom_bar(fill=rgb(1,1,1, alpha=0.5)) +
+      geom_bar(fill = bar_cols$compactness) +
       theme(panel.background = element_blank(),
           plot.background = element_blank(),
           panel.grid.major = element_blank(),
@@ -107,22 +300,22 @@ server <- function(input, output, session) {
           axis.title.x= element_text(color="white"),
           axis.text.x = element_text(color="white"),
           axis.title.y = element_blank(),
-          axis.text.y = element_blank())
+          axis.text.y = element_blank(),
+          legend.position="none")
     
-    p = style(p, hoverinfo="x+y")
-    
-    ggplotly(p) %>% config(displayModeBar = F) %>%
+    p$elementId <- NULL
+
+    ggplotly(p, tooltip = c("text")) %>% config(displayModeBar = F) %>%
       layout(plot_bgcolor='transparent') %>%
       layout(paper_bgcolor='transparent')
-  
-  }
+    }
   )
   
   
   output$vote_efficiency = renderPlotly({
-    p = ggplot(solution_subset(), aes(round(vote_efficiency,2))) +
+    p = ggplot(solution_subset(), aes(round(vote_efficiency,2), text=paste(..count..,"potential maps with","<br>","vote efficiency score of",x))) +
       xlab("Vote Efficiency Score") +
-      geom_bar(fill=rgb(1,1,1, alpha=0.5)) + 
+      geom_bar(fill=bar_cols$vote_efficiency) + 
       theme(panel.background = element_blank(),
             plot.background = element_blank(),
             panel.grid.major = element_blank(),
@@ -131,20 +324,21 @@ server <- function(input, output, session) {
             axis.title.x= element_text(color="white"),
             axis.text.x = element_text(color="white"),
             axis.title.y = element_blank(),
-            axis.text.y = element_blank())
+            axis.text.y = element_blank(),
+            legend.position="none")
+
+    p$elementId <- NULL
     
-    p = style(p, hoverinfo="x+y")
-    
-    ggplotly(p) %>% config(displayModeBar = F) %>%
+    ggplotly(p, tooltip="text") %>% config(displayModeBar = F) %>%
       layout(plot_bgcolor='transparent') %>%
       layout(paper_bgcolor='transparent')
   }
   )
   
   output$cluster_proximity = renderPlotly({
-    p = ggplot(solution_subset(), aes(round(cluster_proximity,2))) +
+    p = ggplot(solution_subset(), aes(round(cluster_proximity,2), text=paste(..count..,"potential maps with","<br>","cluster proximity score of",x))) +
       xlab("Geographic Cluster Score") +
-      geom_bar(fill=rgb(1,1,1, alpha=0.5)) + 
+      geom_bar(fill=bar_cols$cluster_proximity) + 
       theme(panel.background = element_blank(),
             plot.background = element_blank(),
             panel.grid.major = element_blank(),
@@ -153,17 +347,31 @@ server <- function(input, output, session) {
             axis.title.x= element_text(color="white"),
             axis.text.x = element_text(color="white"),
             axis.title.y = element_blank(),
-            axis.text.y = element_blank())
+            axis.text.y = element_blank(),
+            legend.position="none")
     
-    p = style(p, hoverinfo="x+y")
+    p$elementId <- NULL
     
-    ggplotly(p) %>% config(displayModeBar = F) %>%
+    ggplotly(p, tooltip="text") %>% config(displayModeBar = F) %>%
       layout(plot_bgcolor='transparent') %>%
       layout(paper_bgcolor='transparent')
   }
   )
   
   
+  ### LEAFLET MAPS ON RIGHT PANEL
+  values = reactiveValues()
+  values$map1_toggle = FALSE
+
+  
+  output$map1_button = reactive({return(values$map1_toggle)})
+  
+  observeEvent(input$map2_shape_mouseover, {
+    hovertimes = 1
+    mouse = input$map2_shape_mouseover
+    if (mouse$.nonce > 0) print("hi")
+  })
+
   output$map1 = renderLeaflet({
     leaflet(options=leafletOptions(attribution=NULL)) %>%
       addProviderTiles(map_theme,
@@ -177,7 +385,7 @@ server <- function(input, output, session) {
                   fillColor = ~colorFactor(colors, as.factor(assignment))(as.factor(assignment)),
                   highlightOptions = highlightOptions(color = "white", weight = 1,
                                                       bringToFront = TRUE),
-                  label = ~htmlEscape(assignment))
+                  label = ~paste("district:", assignment))
   })
   
   output$map2 = renderLeaflet({
@@ -193,7 +401,7 @@ server <- function(input, output, session) {
                   fillColor = ~colorFactor(colors, as.factor(assignment))(as.factor(assignment)),
                   highlightOptions = highlightOptions(color = "white", weight = 1,
                                                       bringToFront = TRUE),
-                  label = ~htmlEscape(assignment))
+                  label = ~paste("district:", assignment))
   })
   
   output$map3 = renderLeaflet({
@@ -209,7 +417,7 @@ server <- function(input, output, session) {
                   fillColor = ~colorFactor(colors, as.factor(assignment))(as.factor(assignment)),
                   highlightOptions = highlightOptions(color = "white", weight = 1,
                                                       bringToFront = TRUE),
-                  label = ~htmlEscape(assignment))
+                  label = ~paste("district:", assignment))
   })
   
   output$map4 = renderLeaflet({
@@ -225,7 +433,7 @@ server <- function(input, output, session) {
                   fillColor = ~colorFactor(colors, as.factor(assignment))(as.factor(assignment)),
                   highlightOptions = highlightOptions(color = "white", weight = 1,
                                                       bringToFront = TRUE),
-                  label = ~htmlEscape(assignment))
+                  label = ~paste("district:", assignment))
   })
   
   
@@ -242,7 +450,7 @@ server <- function(input, output, session) {
                   fillColor = ~colorFactor(colors, as.factor(assignment))(as.factor(assignment)),
                   highlightOptions = highlightOptions(color = "white", weight = 1,
                                                       bringToFront = TRUE),
-                  label = ~htmlEscape(assignment))
+                  label = ~paste("district:", assignment))
   })
   
   output$map6 = renderLeaflet({
@@ -258,7 +466,7 @@ server <- function(input, output, session) {
                   fillColor = ~colorFactor(colors, as.factor(assignment))(as.factor(assignment)),
                   highlightOptions = highlightOptions(color = "white", weight = 1,
                                                       bringToFront = TRUE),
-                  label = ~htmlEscape(assignment))
+                  label = ~paste("district:", assignment))
   })
 
   
